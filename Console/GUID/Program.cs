f56a85585899df08960a15553343d66aa9072bb0
caigen <caigen@outlook.com>
2020-06-02 13:25:45 +0800

    console playground.

---
+++ Console/GUID/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GUID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; ++i)
+            {
+                System.Guid guid = System.Guid.NewGuid();
+                
+                System.Console.WriteLine(guid.ToString());
+            }
+        }
+    }
+}
