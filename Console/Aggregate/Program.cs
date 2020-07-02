ba297c586ef096102d5ea65fc7b49427b071050d
caigen <caigen@outlook.com>
2020-07-02 13:44:29 +0800

    update

---
+++ Console/Aggregate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aggregate
+{
+    struct data
+    {
+        public string name { set; get; }
+        public string value { set; get; }
+        public data(string name, string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<data> datas = new List<data>();
+            datas.Add(new data("a", "Hello"));
+            datas.Add(new data("b", "World"));
+            datas.Add(new data("a", "Hello2"));
+            datas.Add(new data("b", "World2"));
+
+            int count0 = datas.Where(e => (e.name).Contains("a")).Count() ;
+            Console.WriteLine(count0);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Console/HelloWorld/Program.cs
+++ Console/HelloWorld/Program.cs
@@ -1,14 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace HelloWorld
 {
     class Program
     {
+        static void Test(List<string> list, string tag)
+        {
+            List<string> listFiltered = list.Where(c => c.StartsWith(tag + ":")).ToList();
+
+            foreach (string c in listFiltered)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> list = new List<string>();
+            list.Add("hello:hello");
+            list.Add("world:world");
+            string tag = "hello";
 
-            Console.WriteLine("", typeof(string));
+            Test(list, tag);
         }
     }
 }
